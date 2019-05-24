42990fd51a48795c9dc58871bf2319e6e0aa3a24
Harish Hardy <mj.harishardy@gmail.com>
2019-05-24 12:01:35 +0530

    first commit

--- imageButton/ViewController.swift
+++ imageButton/ViewController.swift
@@ -9,8 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageViewX1: UIImageView!
+    
+    @IBOutlet weak var buttonX1: UIButton!
+    
+    
+    
+    @IBAction func buttonX1(_ sender: Any) {
+        
+        imageViewX1.image = UIImage(named: "steve")
+        
+        
+    }
+    
+    
 
     override func viewDidLoad() {
+        
+        let imageShow = UIImage(named: "apple")?.withRenderingMode(.alwaysOriginal)
+        buttonX1.setImage(imageShow, for: UIControl.State.normal)
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
